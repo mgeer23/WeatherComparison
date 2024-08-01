2ffa72daba3e43d9db65d1f5e16aea6345572246
Mitchell Geer <mitchellgeer23@gmail.com>
2024-08-01 16:59:33 +0100

    Get functions and daily scipt

---
+++ daily_get.py
@@ -0,0 +1,23 @@
+import json
+from get_funcs import met_loc_id, met_5d_forecast, met_past_24h, accu_loc_id, accu_5d_forecast
+
+met_credenhill_id = met_loc_id('Credenhill')
+print(met_credenhill_id)
+
+request = met_5d_forecast(met_credenhill_id)
+if request.startswith('Error'):
+    with open('data/met_5d.jsonl', 'a') as f:
+        f.write(request + '\n')
+else:
+    with open('data/met_5d.jsonl', 'a') as f:
+        f.write(json.dumps(request) + '\n')
+
+
+# with open('data/met_past_24h.jsonl', 'a') as f:
+#     f.write(json.dumps(met_past_24h(met_credenhill_id)) + '\n')
+
+accu_credenhill_id = accu_loc_id('HR47DW')
+print(accu_credenhill_id)
+
+with open('data/accu_5d.jsonl', 'a') as f:
+    f.write(json.dumps(accu_5d_forecast(accu_credenhill_id)) + '\n')
---
+++ get_funcs.py
@@ -0,0 +1,75 @@
+import requests
+from api_keys import accu_key, met_key
+
+met_url = 'http://datapoint.metoffice.gov.uk/public/data/'
+
+
+def accu_loc_id(postcode):
+    postcode_url = 'http://dataservice.accuweather.com/locations/v1/postalcodes/search'
+    payload = {'apikey': accu_key, 'q': postcode}
+
+    r = requests.get(postcode_url, params=payload)
+    if r.status_code == 200:
+        return r.json()[0]['Key']
+    else:
+        print("accu_loc_id error code: " + str(r.status_code))
+        return f'Error code: {r.status_code}'
+
+def accu_5d_forecast(accu_loc_id):
+    accu_5d_url = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/'
+    accu_5d_request = accu_5d_url + accu_loc_id
+    payload = {'apikey': accu_key}
+
+    r = requests.get(accu_5d_request, params = payload)
+    if r.status_code == 200:
+        return r.json()
+    else:
+        print("accu_5d_forecast error code: " + str(r.status_code))
+        return f'Error code: {r.status_code}'
+    
+
+def met_loc_id(location):
+    site_list_resource = 'val/wxfcs/all/json/sitelist'
+    site_list_request = met_url + site_list_resource
+    payload = {'key': met_key}
+
+    r = requests.get(site_list_request, params = payload)
+    if r.status_code == 200:
+        met_locations = r.json()['Locations']['Location']
+        for l in met_locations:
+            if l['name'] == location:
+                met_loc_id = l['id']
+        return met_loc_id
+    else:
+        print("met_loc_id error code: " + str(r.status_code))
+        return f'Error code: {r.status_code}'
+
+def met_5d_forecast(met_loc_id):
+    met_5d_resource = 'val/wxfcs/all/json/'
+    met_5d_request = met_url +met_5d_resource + met_loc_id
+    payload = {'res': 'daily', 'key': met_key}
+
+    r = requests.get(met_5d_request, params=payload)
+    if r.status_code == 200:
+        return r.json()
+    else:
+        print("met_5d_forecast error code: " + str(r.status_code))
+        return f'Error code: {r.status_code}'
+
+def met_past_24h(met_loc_id):
+    met_obs_resource = 'val/wxobs/all/json/'
+    met_obs_request = met_url + met_obs_resource + met_loc_id
+    payload = {'res': 'hourly', 'key': met_key}
+
+    r = requests.get(met_obs_request, params = payload)
+    if r.status_code == 200:
+        return r.json()
+    else:
+        print("met_past_24h error code: " + str(r.status_code))
+        return f'Error code: {r.status_code}'
+
+
+
+
+
+
