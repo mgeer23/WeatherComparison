cd1d22d8fdf4dfaea03c28f96f46e34fc3835899
Mitchell Geer <mitchellgeer23@gmail.com>
2024-08-08 12:51:32 +0100

    met_past_24h sql table add data

---
+++ daily_sql.py
@@ -0,0 +1,6 @@
+from pipeline_funcs import read_last_line, met_past_24h_extract_list, sql_add_data
+import os
+
+json_line = read_last_line('data/met_past_24h.jsonl')
+# json_line = read_last_line(os.getenv('PI_PATH') + 'data/met_past_24h.jsonl')
+sql_add_data('met_past_24h', json_line)
--- pipeline_funcs.py
+++ pipeline_funcs.py
@@ -1,4 +1,8 @@
-import os, json, datetime
+import os, json, mariadb
+
+# ===============================================================
+# ================   READ SAVED JSONL FILES   ===================
+
 
 def read_last_line(file_path):
     """
@@ -15,7 +19,16 @@ def read_last_line(file_path):
         except OSError:
             f.seek(0)
         return json.loads(f.readline().decode())
-    
+
+def read_every_line(file_path):
+    with open(file_path, 'rb') as f:
+        jsons = f.readlines()
+        for j in jsons:
+            yield json.loads(j)
+
+# ===============================================================
+# ===============   DATA EXTRACTION FROM JSON   =================
+
 def met_past_24h_extract(json_line):
     """
     Takes a json line of the met past 24 hour data and generates
@@ -27,14 +40,15 @@ def met_past_24h_extract(json_line):
     days = json_line['SiteRep']['DV']['Location']['Period']
     for day in days:
         date = day['value'][:-1]
-        split_date = str.split(date, '-')
+        # split_date = str.split(date, '-')
         for h in day['Rep']:
             temp = h['T']
             weather_code = h['W']
             precip_bool = int(int(weather_code) > 8)
-            hour = int(h['$']) / 60
-            dt = datetime.datetime(int(split_date[0]), int(split_date[1]), int(split_date[2]), hour)
-            data_row = [location, dt, temp, weather_code, precip_bool]
+            hour = int(int(h['$']) / 60)
+            # dt = datetime.datetime(int(split_date[0]), int(split_date[1]), int(split_date[2]), hour)
+            stringtime = f"{date} {hour}:00:00"
+            data_row = [location, stringtime, temp, weather_code, precip_bool]
             yield data_row
 
 def met_past_24h_extract_list(json_line):
@@ -49,25 +63,53 @@ def met_past_24h_extract_list(json_line):
     days = json_line['SiteRep']['DV']['Location']['Period']
     for day in days:
         date = day['value'][:-1]
-        split_date = str.split(date, '-')
+        # split_date = str.split(date, '-')
         for h in day['Rep']:
             temp = h['T']
             weather_code = h['W']
             precip_bool = int(int(weather_code) > 8)
-            hour = int(h['$']) / 60
-            dt = datetime.datetime(int(split_date[0]), \
-                                   int(split_date[1]), \
-                                    int(split_date[2]), hour)
-            return_list.append([location, dt, temp, weather_code, precip_bool])
+            hour = int(int(h['$']) / 60)
+            # dt = datetime.datetime(int(split_date[0]), \
+            #                        int(split_date[1]), \
+            #                         int(split_date[2]), hour)
+            stringtime = f"{date} {hour}:00:00"
+            return_list.append([location, stringtime, temp, weather_code, precip_bool])
     return return_list
 
+# ===============================================================
+# ===================   MARIADB FUNCTIONS   =====================
 
+col_name_dict = {'met_past_24h' : ['(location, date_time, temp, weather_code, precip_bool)', 
+                                   '(?, ?, ?, ?, ?)']}
 
-line = read_last_line('data/met_past_24h.jsonl')
-print(line)
-print(type(line))
+extract_dict = {'met_past_24h' : met_past_24h_extract_list}
 
-print(met_past_24h_extract(line))
+def sql_add_data(table_name, json_line):
+    col_names = col_name_dict[table_name]
+    insert_query = f"""
+                    INSERT INTO {table_name} {col_names[0]}
+                    VALUES {col_names[1]}
+                    """
+    new_values = extract_dict['met_past_24h'](json_line)
 
-for h in met_past_24h_extract(line):
-    print(h)
+    try:
+        with mariadb.connect(host = 'localhost',
+                    user = os.getenv('MYSQL_USER'),
+                    password = os.getenv('MYSQL_PASS'),
+                    database = 'weather_comp') as connection:
+            cur = connection.cursor()
+            cur.executemany(insert_query, new_values)
+            connection.commit()
+    except mariadb.Error as e:
+        print(e)
+
+# for line in read_every_line('data/met_past_24h.jsonl'):
+#     print(line)
+
+# line = read_last_line('data/met_past_24h.jsonl')
+# sql_add_data('met_past_24h', line)
+
+# print(met_past_24h_extract(line))
+
+# for h in met_past_24h_extract(line):
+#     print(h)
